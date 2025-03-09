fab56970c68a7bfa3d2479ec23c5bfe43847e4ae
KebDello <kebdeloyt@gmail.com>
2025-03-09 16:31:24 +0400

    login page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const restApiUrl = "http://localhost:8080/api";
-
+export default restApiUrl;
 function fetchUserProfile() {
     const token = localStorage.getItem('token');
 
--- pages/registration/registration.js
+++ pages/registration/registration.js
@@ -1,3 +1,5 @@
+import restApiUrl from "../../js/script"
+
 (() => {
     'use strict'
   
@@ -7,9 +9,49 @@
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
+        const name = document.querySelector(".name").value;
+        const surname = document.querySelector(".surname").value;
+        const email = document.querySelector(".email").value;
+        const username = document.querySelector(".username").value;
+        const password = document.querySelector(".passwod").value;
+
+
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
+        } else{
+            event.preventDefault()
+            event.stopPropagation()
+            const newUser = {
+                name,
+                surname,
+                email,
+                username,
+                password
+            }
+
+            axios.post(restApiUrl + "/auth/register", newUser)
+            .then((response)=>{
+                console.log(response.data);
+                if(response.data.message){
+                    alert("Успешная регестрация:" + response.data.message);
+                }else{
+                    alert("Регестрация прошла успешно!")
+                }
+
+                window.location.href = "/pages/logIn/login.html"
+            })
+            .catch((error) =>{
+                xonsole.error(error);
+
+                if(error.response){
+                    alert("Ошибка при регестрации:"+ error.response.data);
+                }else if(error.request){
+                    alert("Ошиьбка запроса: Нет ответа от сервера.")
+                }else{
+                    alert("Неизвестная ошибка: " + error.message)
+                }
+            })
         }
   
         form.classList.add('was-validated')
